5028d27920cf52f1493313b60396c99173229492
Zavians <kenzatekkom20@gmail.com>
2023-11-10 14:26:24 +0700

    Tugas GUI

---
+++ Form1.Designer.vb
@@ -0,0 +1,348 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.CtoR = New System.Windows.Forms.Button()
+        Me.CtoF = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.CtoK = New System.Windows.Forms.Button()
+        Me.RtoC = New System.Windows.Forms.Button()
+        Me.RtoF = New System.Windows.Forms.Button()
+        Me.RtoK = New System.Windows.Forms.Button()
+        Me.FtoC = New System.Windows.Forms.Button()
+        Me.FtoR = New System.Windows.Forms.Button()
+        Me.FtoK = New System.Windows.Forms.Button()
+        Me.KtoR = New System.Windows.Forms.Button()
+        Me.KtoC = New System.Windows.Forms.Button()
+        Me.KtoF = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'CtoR
+        '
+        Me.CtoR.Location = New System.Drawing.Point(218, 320)
+        Me.CtoR.Name = "CtoR"
+        Me.CtoR.Size = New System.Drawing.Size(75, 23)
+        Me.CtoR.TabIndex = 0
+        Me.CtoR.Text = "C to R"
+        Me.CtoR.UseVisualStyleBackColor = True
+        '
+        'CtoF
+        '
+        Me.CtoF.Location = New System.Drawing.Point(218, 358)
+        Me.CtoF.Name = "CtoF"
+        Me.CtoF.Size = New System.Drawing.Size(75, 23)
+        Me.CtoF.TabIndex = 1
+        Me.CtoF.Text = "C to F"
+        Me.CtoF.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(325, 255)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(186, 27)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(275, 194)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(318, 29)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "APLIKASI KONVERSI SUHU"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(27, 25)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(217, 25)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Anggota Kelompok 21 :"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(28, 65)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(184, 20)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "Kenza Vianda Dwiputra"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(28, 98)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(180, 20)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "M. Yahya Oktariansyah"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(28, 133)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(123, 20)
+        Me.Label6.TabIndex = 9
+        Me.Label6.Text = "M. Fachru Razi"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(28, 167)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(106, 20)
+        Me.Label7.TabIndex = 10
+        Me.Label7.Text = "Novita Auliya"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(526, 25)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(125, 25)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "Keterangan :"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(585, 65)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(97, 20)
+        Me.Label8.TabIndex = 12
+        Me.Label8.Text = "C = Celcius"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label9.Location = New System.Drawing.Point(699, 65)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(118, 20)
+        Me.Label9.TabIndex = 13
+        Me.Label9.Text = "F = Fahrenheit"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(86, 194)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(0, 16)
+        Me.Label10.TabIndex = 14
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label11.Location = New System.Drawing.Point(383, 65)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(105, 20)
+        Me.Label11.TabIndex = 15
+        Me.Label11.Text = " R = Reamur"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label12.Location = New System.Drawing.Point(494, 65)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(85, 20)
+        Me.Label12.TabIndex = 16
+        Me.Label12.Text = "K = Kelvin"
+        '
+        'CtoK
+        '
+        Me.CtoK.Location = New System.Drawing.Point(218, 397)
+        Me.CtoK.Name = "CtoK"
+        Me.CtoK.Size = New System.Drawing.Size(75, 23)
+        Me.CtoK.TabIndex = 17
+        Me.CtoK.Text = "C to K"
+        Me.CtoK.UseVisualStyleBackColor = True
+        '
+        'RtoC
+        '
+        Me.RtoC.Location = New System.Drawing.Point(325, 320)
+        Me.RtoC.Name = "RtoC"
+        Me.RtoC.Size = New System.Drawing.Size(75, 23)
+        Me.RtoC.TabIndex = 18
+        Me.RtoC.Text = "R to C"
+        Me.RtoC.UseVisualStyleBackColor = True
+        '
+        'RtoF
+        '
+        Me.RtoF.Location = New System.Drawing.Point(325, 358)
+        Me.RtoF.Name = "RtoF"
+        Me.RtoF.Size = New System.Drawing.Size(75, 23)
+        Me.RtoF.TabIndex = 19
+        Me.RtoF.Text = "R to F"
+        Me.RtoF.UseVisualStyleBackColor = True
+        '
+        'RtoK
+        '
+        Me.RtoK.Location = New System.Drawing.Point(325, 397)
+        Me.RtoK.Name = "RtoK"
+        Me.RtoK.Size = New System.Drawing.Size(75, 23)
+        Me.RtoK.TabIndex = 20
+        Me.RtoK.Text = "R to K"
+        Me.RtoK.UseVisualStyleBackColor = True
+        '
+        'FtoC
+        '
+        Me.FtoC.Location = New System.Drawing.Point(436, 397)
+        Me.FtoC.Name = "FtoC"
+        Me.FtoC.Size = New System.Drawing.Size(75, 23)
+        Me.FtoC.TabIndex = 21
+        Me.FtoC.Text = "F to C"
+        Me.FtoC.UseVisualStyleBackColor = True
+        '
+        'FtoR
+        '
+        Me.FtoR.Location = New System.Drawing.Point(436, 358)
+        Me.FtoR.Name = "FtoR"
+        Me.FtoR.Size = New System.Drawing.Size(75, 23)
+        Me.FtoR.TabIndex = 22
+        Me.FtoR.Text = "F to R"
+        Me.FtoR.UseVisualStyleBackColor = True
+        '
+        'FtoK
+        '
+        Me.FtoK.Location = New System.Drawing.Point(436, 320)
+        Me.FtoK.Name = "FtoK"
+        Me.FtoK.Size = New System.Drawing.Size(75, 23)
+        Me.FtoK.TabIndex = 23
+        Me.FtoK.Text = "F to K"
+        Me.FtoK.UseVisualStyleBackColor = True
+        '
+        'KtoR
+        '
+        Me.KtoR.Location = New System.Drawing.Point(548, 320)
+        Me.KtoR.Name = "KtoR"
+        Me.KtoR.Size = New System.Drawing.Size(75, 23)
+        Me.KtoR.TabIndex = 24
+        Me.KtoR.Text = "K to R"
+        Me.KtoR.UseVisualStyleBackColor = True
+        '
+        'KtoC
+        '
+        Me.KtoC.Location = New System.Drawing.Point(548, 397)
+        Me.KtoC.Name = "KtoC"
+        Me.KtoC.Size = New System.Drawing.Size(75, 23)
+        Me.KtoC.TabIndex = 25
+        Me.KtoC.Text = "K to C"
+        Me.KtoC.UseVisualStyleBackColor = True
+        '
+        'KtoF
+        '
+        Me.KtoF.Location = New System.Drawing.Point(548, 358)
+        Me.KtoF.Name = "KtoF"
+        Me.KtoF.Size = New System.Drawing.Size(75, 23)
+        Me.KtoF.TabIndex = 26
+        Me.KtoF.Text = "K to F"
+        Me.KtoF.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(840, 450)
+        Me.Controls.Add(Me.KtoF)
+        Me.Controls.Add(Me.KtoC)
+        Me.Controls.Add(Me.KtoR)
+        Me.Controls.Add(Me.FtoK)
+        Me.Controls.Add(Me.FtoR)
+        Me.Controls.Add(Me.FtoC)
+        Me.Controls.Add(Me.RtoK)
+        Me.Controls.Add(Me.RtoF)
+        Me.Controls.Add(Me.RtoC)
+        Me.Controls.Add(Me.CtoK)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.CtoF)
+        Me.Controls.Add(Me.CtoR)
+        Me.Name = "Form1"
+        Me.Text = "S"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents CtoR As Button
+    Friend WithEvents CtoF As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents CtoK As Button
+    Friend WithEvents RtoC As Button
+    Friend WithEvents RtoF As Button
+    Friend WithEvents RtoK As Button
+    Friend WithEvents FtoC As Button
+    Friend WithEvents FtoR As Button
+    Friend WithEvents FtoK As Button
+    Friend WithEvents KtoR As Button
+    Friend WithEvents KtoC As Button
+    Friend WithEvents KtoF As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,167 @@
+﻿Imports Mod8_Tugas_GUI
+Imports System.Reflection.Emit
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+    End Sub
+
+
+
+
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles CtoR.Click
+
+        Dim objConv As New Mod8_Tugas_GUI.TempConvComp
+
+        Dim c As Double
+
+        c = CDbl(TextBox1.Text)
+
+        TextBox1.Text = objConv.cToReamur(c)
+        Label1.Text = "Ini Celcius Ke Reamur"
+
+    End Sub
+
+
+
+    Private Sub button2_Click(sender As Object, e As EventArgs) Handles CtoF.Click
+
+        Dim objConv As New Mod8_Tugas_GUI.TempConvComp
+
+        Dim c As Double
+
+        c = CDbl(TextBox1.Text)
+
+        TextBox1.Text = objConv.cToF(c)
+        Label1.Text = "Ini Celcius Ke Kelvin"
+
+    End Sub
+
+    Private Sub button3_Click(sender As Object, e As EventArgs) Handles CtoK.Click
+
+        Dim objConv As New Mod8_Tugas_GUI.TempConvComp
+
+        Dim c As Double
+
+        c = CDbl(TextBox1.Text)
+
+        TextBox1.Text = objConv.cToKelvin(c)
+        Label1.Text = "Celcius"
+
+    End Sub
+
+    Private Sub button4_Click(sender As Object, e As EventArgs) Handles RtoC.Click
+
+        Dim objConv As New Mod8_Tugas_GUI.TempConvComp
+
+        Dim reamur As Double
+
+        reamur = CDbl(TextBox1.Text)
+
+        TextBox1.Text = objConv.reamurToC(reamur)
+        Label1.Text = "Ini Reamur Ke Celcius"
+
+    End Sub
+
+    Private Sub button5_Click(sender As Object, e As EventArgs) Handles RtoF.Click
+
+        Dim objConv As New Mod8_Tugas_GUI.TempConvComp
+
+        Dim reamur As Double
+
+        reamur = CDbl(TextBox1.Text)
+
+        TextBox1.Text = objConv.reamurToF(reamur)
+        Label1.Text = "Ini Reamur Ke Fahreinheit"
+
+    End Sub
+
+    Private Sub button6_Click(sender As Object, e As EventArgs) Handles RtoK.Click
+
+        Dim objConv As New Mod8_Tugas_GUI.TempConvComp
+
+        Dim reamur As Double
+        reamur = CDbl(TextBox1.Text)
+        TextBox1.Text = objConv.reamurToKelvin(reamur)
+        Label1.Text = "Ini Reamur Ke Kelvin"
+    End Sub
+
+    Private Sub button7_Click(sender As Object, e As EventArgs) Handles FtoC.Click
+
+        Dim objConv As New Mod8_Tugas_GUI.TempConvComp
+
+        Dim f As Double
+        f = CDbl(TextBox1.Text)
+        TextBox1.Text = objConv.fToC(f)
+        Label1.Text = "Ini Fahreinheit Ke Celcius"
+    End Sub
+
+    Private Sub button8_Click(sender As Object, e As EventArgs) Handles FtoR.Click
+
+        Dim objConv As New Mod8_Tugas_GUI.TempConvComp
+
+        Dim f As Double
+        f = CDbl(TextBox1.Text)
+        TextBox1.Text = objConv.fToReamur(f)
+        Label1.Text = "Ini Fahreinheit Ke Reamur"
+    End Sub
+
+    Private Sub button9_Click(sender As Object, e As EventArgs) Handles FtoK.Click
+
+        Dim objConv As New Mod8_Tugas_GUI.TempConvComp
+
+        Dim f As Double
+        f = CDbl(TextBox1.Text)
+        TextBox1.Text = objConv.fToKelvin(f)
+        Label1.Text = "Ini Fahreinheit Ke Kelvin"
+    End Sub
+
+    Private Sub button10_Click(sender As Object, e As EventArgs) Handles KtoC.Click
+
+        Dim objConv As New Mod8_Tugas_GUI.TempConvComp
+
+        Dim kelvin As Double
+        kelvin = CDbl(TextBox1.Text)
+        TextBox1.Text = objConv.kelvinToC(kelvin)
+        Label1.Text = "Ini Kelvin Ke Celcius"
+    End Sub
+
+    Private Sub button11_Click(sender As Object, e As EventArgs) Handles KtoR.Click
+
+        Dim objConv As New Mod8_Tugas_GUI.TempConvComp
+
+        Dim kelvin As Double
+        kelvin = CDbl(TextBox1.Text)
+        TextBox1.Text = objConv.kelvinToC(kelvin)
+        Label1.Text = "Ini Kelvin Ke Reamur"
+    End Sub
+
+    Private Sub button12_Click(sender As Object, e As EventArgs) Handles KtoF.Click
+
+        Dim objConv As New Mod8_Tugas_GUI.TempConvComp
+
+        Dim kelvin As Double
+        kelvin = CDbl(TextBox1.Text)
+        TextBox1.Text = objConv.kelvinToF(kelvin)
+        Label1.Text = "Ini Kelvin Ke Fahrenheit"
+    End Sub
+
+
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+End Class
